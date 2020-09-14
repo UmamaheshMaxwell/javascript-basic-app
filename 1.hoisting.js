a015460ca32aae5f42b4370836d4c569602fff1f
Umamaheswararao Meka <maheshmeka16@gmail.com>
2020-09-14 08:46:50 +0530

    added basic concepts

---
+++ 1.hoisting.js
@@ -0,0 +1,15 @@
+b()
+console.log(a)
+
+a = undefined;
+
+var a ="Welcome to Naresh IT"
+
+function b(){
+    console.log('called b!!')
+}
+
+
+
+
+
---
+++ 2.undefined.js
@@ -0,0 +1,28 @@
+/*----------------Example 1-------------------------*/
+var a;
+console.log(a)
+
+if(a === undefined) {
+    console.log('a is undefined')
+}
+else {
+    console.log('a is defined')
+}
+
+/*----------------Example 2-------------------------*/
+
+function b(){
+    console.log('b is called')
+}
+
+b();
+
+console.log(a);
+var a = "Welcome to Naresh IT";
+console.log(a)
+
+/*----------------Example 3-------------------------*/
+var a=10; //kept a in memory and assign a default value undefined a =undefined , a =udefined
+var a // Javascript engine already read this line as a first step
+console.log(a)
+
